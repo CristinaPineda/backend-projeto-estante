9edf1c51be5e394dcaf660472f2e1bd97184dd16
cegalvao92 <cegalvao13@gmail.com>
2022-03-15 22:18:40 -0300

    criação pasta models e estante.js

---
+++ models/estante.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const estanteModel = new mongoose.Schema({
+    // nome: { type: String, required: true },
+    // imagemUrl: { type: String, required: true },
+});
+
+module.exports = estanteModel;
